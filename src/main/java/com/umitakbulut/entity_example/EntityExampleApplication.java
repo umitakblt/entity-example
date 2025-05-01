9fcba93ef7d2a2e0babd732ba7422232280004e9
deneme <com.deneme>
2025-05-01 20:55:14 +0300

    Initial commit

---
+++ src/main/java/com/umitakbulut/entity_example/EntityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.umitakbulut.entity_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umitakbulut/entity_example/EntityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umitakbulut.entity_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
