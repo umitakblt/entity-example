9e898cec90598c1e8114ed8b049ed80a0c962490
ümit akbulut <77067747+umitakblt@users.noreply.github.com>
2025-05-01 22:14:19 +0300

    CreateUserRequest, UserResponse added.

---
+++ src/main/java/com/umitakbulut/entity_example/dto/response/UserResponse.java
@@ -0,0 +1,62 @@
+package com.umitakbulut.entity_example.dto.response;
+
+import java.io.Serializable;
+
+public class UserResponse implements Serializable {
+    private String name;
+    private String surname;
+    private String email;
+    private String phoneNumber;
+
+    public UserResponse() {
+    }
+
+    public UserResponse(String name, String surname, String email, String phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "UserResponse{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
