1088742821d0df5b2fc065f07e761f38b2bf2e25
deneme <com.deneme>
2025-05-01 21:10:03 +0300

    User entity added!

---
+++ src/main/java/com/umitakbulut/entity_example/entity/User.java
@@ -0,0 +1,106 @@
+package com.umitakbulut.entity_example.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "sur_name", nullable = false)
+    private String surName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false, unique = true)
+    private String phoneNumber;
+
+    @CreationTimestamp
+    @Column(name = "created", nullable = false)
+    private Instant created;
+
+    @UpdateTimestamp
+    @Column(name = "updated", nullable = false)
+    private Instant updated;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String surName, String email, String phoneNumber, Instant created, Instant updated) {
+        this.id = id;
+        this.name = name;
+        this.surName = surName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+
+    public Instant getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Instant updated) {
+        this.updated = updated;
+    }
+}
